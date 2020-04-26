da31891ec89a9d2b0160a294c1ae4df9f063003c
Ricardo <ricardoshhan@gmail.com>
2020-04-26 16:37:57 -0300

    single file for path shortener

--- url-shortener/main.go
+++ url-shortener/main.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"fmt"
+	yaml "gopkg.in/yaml.v2"
 	"net/http"
-
-	urlshort "github.com/ricardohan93/gophercises/url-shortener"
 )
 
 func main() {
@@ -15,19 +14,78 @@ func main() {
 		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
 	}
 
-	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
+	mapHandler := mapHandlerFunc(pathsToUrls, mux)
 
-	// yaml := `
-	// 	- path: /urlshort
-	// 	url: https://github.com/gophercises/urlshort
-	// 	-path: /urlshort-final
-	// 	url: https://github.com/gophercises/urlshort/tree/final
-	// `
+	yaml := `
+- path: /urlshort
+  url: https://github.com/gophercises/urlshort
+- path: /urlshort-final
+  url: https://github.com/gophercises/urlshort/tree/solution
+`
 
-	// yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	yamlHandler, err := yAMLHandlerFunc([]byte(yaml), mapHandler)
+	if err != nil {
+		panic(err)
+	}
+
 	fmt.Println("Starting the server on :8080")
 	http.ListenAndServe(":8080", yamlHandler)
+}
+
+func mapHandlerFunc(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// get the path from the incoming URL
+		path := r.URL.Path
+		// check if the path exist in our mapping
+		if dest, ok := pathsToUrls[path]; ok {
+			// if the path exists, we redirect the user to that URL (dest)
+			http.Redirect(w, r, dest, http.StatusFound)
+			return
+		}
+		fallback.ServeHTTP(w, r)
+	}
+}
+
+type pathURL struct {
+	Path string `yaml:"path"`
+	URL  string `yaml:"url"`
+}
+
+func yAMLHandlerFunc(yamlBytes []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	// parse the yaml
+	pathUrls, err := parseYaml(yamlBytes)
+	if err != nil {
+		return nil, err
+	}
+	// convert yaml array into map
+	pathsToUrls := buildMap(pathUrls)
+	// return a mapHandler
+	return mapHandlerFunc(pathsToUrls, fallback), nil
+}
+
+func buildMap(pathUrls []pathURL) map[string]string {
+	pathsToUrls := make(map[string]string)
+	for _, pu := range pathUrls {
+		pathsToUrls[pu.Path] = pu.URL
+	}
+	return pathsToUrls
+}
+
+func parseYaml(data []byte) ([]pathURL, error) {
+	var pathUrls []pathURL
+	err := yaml.Unmarshal(data, &pathUrls)
+	if err != nil {
+		return nil, err
+	}
+	return pathUrls, nil
+}
+
+func defaultMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+	return mux
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, world!")
 }
